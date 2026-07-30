3552d2acf37793877a9bd5bcf6839e72e115f73d
isaac brown <isaac.brown154@outlook.com>
2026-07-30 22:08:18 +0930

    just fixed up some spelling problems in book.py and added some comments

--- scrapers/models/book.py
+++ scrapers/models/book.py
@@ -1,4 +1,7 @@
-
+"""
+DEV: Isaac M.D. Brown
+Last Iteration: 30/07/2026
+"""
 
 
 
@@ -6,27 +9,27 @@
 class Book:
     """A simple model of a book"""
 
-    # default instance vairbles
+    # default instance variables
     DEF_TITLE: str = "Untitled"
-    DEF_AURTHER: str = "Nameless"
+    DEF_AUTHOR: str = "Nameless"
     DEF_RATING: str = "Zero"
     
-    # intsance vairbles type hinting
+    # instance variables type hinting
     title: str
-    aurther: str
+    author: str
     rating: str
 
-    def __init__(self, title: str = DEF_TITLE, aurther: str = DEF_AURTHER, rating: str = DEF_RATING):
+    def __init__(self, title: str = DEF_TITLE, author: str = DEF_AUTHOR, rating: str = DEF_RATING):
         """Constructor for book object"""
         self.title = title
-        self.aurther = aurther
+        self.aurthor = author
         self.rating = rating
     
     def __str__(self):
-        """Overide for __str__ method, returns a formatted str"""
+        """Override for __str__ method, returns a formatted str"""
         return str(
                 f"Title: {self.title}\n" +
-                f"Aurther: {self.aurther}\n" +
+                f"Author: {self.author}\n" +
                 f"Rating: {self.rating}\n"
                 )
         
