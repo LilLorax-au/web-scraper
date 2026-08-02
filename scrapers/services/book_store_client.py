09d0a466f7dc4c99ea4690c09a7b996b6088a606
isaac brown <isaac.brown154@outlook.com>
2026-08-02 23:15:44 +0930

    further work done on BookStoreClient

--- scrapers/services/book_store_client.py
+++ scrapers/services/book_store_client.py
@@ -10,6 +10,28 @@ from ..models import Book
 
 
 class BookStoreClient:
-    """Used to pull data from books.toscrape.com"""
+    """Used to pull data from books.toscrape.com, storing the data as Book's"""
     
+    # Constants
     BASE_URL: str = "https://books.toscrape.com/"
+    CATEGORY_ROUTE: str = "catalogue/category/books/"
+    
+    # Default instance variables
+    DEF_CATEGORY: str = ""
+    DEF_URL_OVERRIDE: str | None = None
+    
+    # Instance variables type hinting
+    category: str
+    url_override: str
+    url: str
+
+    def __init__(self, category: str = DEF_CATEGORY, url_override: str | None = DEF_URL_OVERRIDE):
+        """Constructor for BookStoreClient"""
+        if category:
+        self.category = category
+        
+
+        
+        
+
+
